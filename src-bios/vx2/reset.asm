39ceafe7db796c30fe51fa7e95b95936f3b84f52
Philip Searle <philip@philip-searle.me.uk>
2025-12-06 22:06:30 +0000

    Add fatal boot error handler

--- src-bios/vx2/reset.asm
+++ src-bios/vx2/reset.asm
@@ -5,10 +5,54 @@ RESET		PROGRAM	OutFile=reset.obj
 		include	"segments.inc"
 		include	"segments/bda.inc"
 		include	"segments/ivt.inc"
+		include	"ports.inc"
 
-		PUBLIC	Reset_Compat
+		PUBLIC	Reset_Actual
 
-Reset_Compat	PROC
-		ENDPROC	Reset_Compat
+; ═╡ FatalError ╞══════════════════════════════════════════╡ nostack noreturn ╞═
+;
+; Called by reset code to indicate startup cannot proceed.
+;
+; Writes an error code to port 28F8h then continually blinks the network LED.
+; Also writes to ports 4FAh and 8FAh?
+;
+; On entry:
+;   AL = Error code
+;   SI → Scratch memory
+; ══════════════════════════════════════════════════════════════════════════════
+FatalError	PROC
+		cli
+		mov	dx, PORT_UNKNOWN_28F8	; ???
+		out	dx, al
+
+		mov	ah, MISC_IO_UNK40 | MISC_IO_NETLED
+.blinkLed	mov	al, 0
+		mov	dx, PORT_UNKNOWN_4F8	; ???
+		out	dx, al
+		inc	dx			; Advance to PORT_UNKNOWN_4F9
+		mov	al, ah,CODE=LONG
+		out	dx, al
+
+		xor	cx, cx,CODE=LONG			; Max iterations
+.wait		mov	[si], cx		; Store iteration count
+		mov	dx, PORT_UNKNOWN_4FA
+		mov	al, cl,CODE=LONG
+		out	dx, al			; ??? lo byte of loop count
+		in	al, dx			; readback value
+		mov	dx, PORT_UNKNOWN_8FA
+		mov	al, cl,CODE=LONG
+		out	dx, al			; ??? lo byte of loop count
+		in	al, dx			; readback value
+		mov	dx, [si]
+		mov	dx, [si]		; ??? why read twice
+		loop	.wait
+		xor	ah, MISC_IO_NETLED	; toggle LED
+		jmp	.blinkLed
+		ENDPROC	FatalError
+
+; ═╡ Reset_Actual ╞════════════════════════════════════════╡ nostack noreturn ╞═
+; ══════════════════════════════════════════════════════════════════════════════
+Reset_Actual	PROC
+		ENDPROC	Reset_Actual
 
 ENDPROGRAM	RESET
--- src-bios/vx2/rom_trailer.asm
+++ src-bios/vx2/rom_trailer.asm
@@ -5,24 +5,28 @@ ROM_TRAILER	PROGRAM	OutFile=rom_trailer.obj
 		include	"segments.inc"
 		include	"bios-version.inc"
 
-		EXTERN	Reset_Compat
+		EXTERN	Reset_Actual
 
 		PUBLIC	Reset, ReleaseMarker, MachineId
 
-; ---------------------------------------------------------------------------
+; ══════════════════════════════════════════════════════════════════════════════
 ; Power-On Reset Vector at F000:FFF0
-; ---------------------------------------------------------------------------
+; [Public] CPU reset vector is top of address space minus 16 bytes.
+; AT BIOS calls this P_O_R (POWER ON RESET).
+; Must be a far jump to set CS to the correct value.
+; ══════════════════════════════════════════════════════════════════════════════
+Reset		jmpf	Reset_Actual
 
-		; [Public] CPU reset vector is top of address space minus 16 bytes
-		; AT BIOS calls this P_O_R (POWER ON RESET)
-		; Must be a far jump to set CS to the correct value.
-Reset		jmpf	Reset_Compat
+; ══════════════════════════════════════════════════════════════════════════════
+; BIOS identification in last 11 bytes
+; ══════════════════════════════════════════════════════════════════════════════
 
-		; [Public] IBM BIOS stores ROM date at F000:FFF5 (AT BIOS calls this 'RELEASE MARKER')
+		; [Public] IBM BIOS stores ROM date at F000:FFF5
+		; (AT BIOS calls this 'RELEASE MARKER')
 ReleaseMarker	db	%BIOS_DATE
 		db	00h		; Unused byte
 
-		; [Public] IBM BIOS stores machine identification byte at F000:FFFE
+		; [Public] IBM BIOS stores machine ID byte at F000:FFFE
 MachineId	db	0FCh		; Model FC == IBM AT (6 MHz)
 		db	00		; Submodel 00 == no specific submodel
 
