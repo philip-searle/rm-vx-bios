14ed5abdaeaca6a32160d71a35ca20eba234f8fc
Philip Searle <philip@philip-searle.me.uk>
2025-12-07 00:07:03 +0000

    Start on Reset_Actual

--- src-bios/vx2/reset.asm
+++ src-bios/vx2/reset.asm
@@ -5,6 +5,8 @@ RESET		PROGRAM	OutFile=reset.obj
 		include	"segments.inc"
 		include	"segments/bda.inc"
 		include	"segments/ivt.inc"
+		include	"cmos.inc"
+		include	"keyboard.inc"
 		include	"ports.inc"
 
 		PUBLIC	Reset_Actual
@@ -46,13 +48,44 @@ FatalError	PROC
 		mov	dx, [si]
 		mov	dx, [si]		; ??? why read twice
 		loop	.wait
+
 		xor	ah, MISC_IO_NETLED	; toggle LED
 		jmp	.blinkLed
 		ENDPROC	FatalError
 
 ; ═╡ Reset_Actual ╞════════════════════════════════════════╡ nostack noreturn ╞═
+;
+; First code executed after jumping from the reset vector.
 ; ══════════════════════════════════════════════════════════════════════════════
 Reset_Actual	PROC
+		cli
+		in	al, PORT_UNKNOWN_81	; clear flipflop?
+		mov	dx, PORT_UNKNOWN_CFF
+		mov	al, 80h			; ???
+		out	dx, al
+
+; ──────────────────────────────────────────────────────────────────────────────
+		DiagOut	Code=01h, Subcode=01h	; Check shutdown reason
+
+		; Store status register in CH for later
+		in	al, PORT_KBC_STATUS
+		mov_	ch, al
+
+		; Read shutdown reason to CL
+		mov	al, CMOS_SHUTDOWN_REASON | NMI_DISABLE
+		out	PORT_CMOS_ADDRESS, al
+		Delay	2
+		in	al, PORT_CMOS_DATA
+		Delay	2
+		mov_	cl, al
+
+		; Reset shutdown reason
+		xor_	al, al
+		out	PORT_CMOS_DATA, al
+
+		; Output shutdown reason to diagnostic port
+		; [Undoc] not mentioned in [Service B.1]
+		DiagOut	Subcode=cl
 		ENDPROC	Reset_Actual
 
 ENDPROGRAM	RESET
